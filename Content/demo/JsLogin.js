7ce4daccb7d239352fc568d7fd2fcbd3deeb7361
EreKraken <rtayler561@gmail.com>
2021-01-21 10:03:14 -0400

    PrimerCommit

---
+++ Content/demo/JsLogin.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+    $("form[name='login']").validate({
+        rules: {
+            Login: {
+                required: true,
+                Login: true
+            },
+            Contraseña: {
+                required: true,
+            },
+            Tipo: {
+                required: true,
+            }
+        },
+        messages: {
+            Login: "Por favor, introduce un usario válida",
+            Contraseña: {
+                required: "Por favor, ingrese contraseña",
+            },
+            TipoUsuario:"Obligatorio"
+        },
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+});
+
+
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArcoIris.MvcApplication" Language="C#" %>
